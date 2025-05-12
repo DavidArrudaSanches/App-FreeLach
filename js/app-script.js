09bdfd4333f8d26cd862babcb98f14ee28cb9a72
DavidArrudaSanches <david.sanches2750@gmail.com>
2025-05-12 10:38:05 -0300

    Criando JS do App Tarefas

---
+++ js/app-script.js
@@ -0,0 +1,49 @@
+const definirSaudacao= ()=>{
+    const header = document.querySelector('#saudacaoHeader');
+    setInterval(()=>{
+        const now = new Date ();
+        const horas = now.getHours();
+        const minutos = now.getMinutes()
+        const segundos = now.getSeconds()
+        const dataAtual = now.toLocaleDateString('pt-BR')
+        
+            let saudacao;
+            if (horas >= 6 && horas <12){
+                saudacao = 'Bom dia!';
+            }else if(horas >= 12 && horas <18){
+                saudacao='Boa Tarde';
+            }else{
+                saudacaoa='Boa noite!'
+            }
+        
+            header.innerHTML = `<span>${saudacao}</span><span>${dataAtual} ${horas}:${minutos}:${segundos}</span>`
+    }, 1000
+)
+}
+definirSaudacao()
+
+const carregarTarefas = ()=>{
+    const taskList = document.querySelector('#taskList')
+    taskItem.classList.add('task-Item')
+    if (tarefa.concluida){
+        taskItem.classList.add('concluida')
+    }
+    taskItem.innerHTML = tarefa.html
+    taskList.appendChild(taskItem)
+
+    taskItem.querySelector('.complete-btn').addEventListener('click', function(){
+        marcarComoConcluida(this)
+    })
+    
+    taskItem.querySelector('.edit-btn').addEventListener('click', function(){
+        editarTarefa(this)
+    })
+    
+    taskItem.querySelector('.delete-btn').addEventListener('click', function(){
+        excluirTarefa(this)
+    })
+}
+
+const adicionarTarefa = () => {
+    
+}
